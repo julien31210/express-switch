fe436958a9e2721f9a26cfc891a8e596b239a227
Julien <julien.linder@gmail.com>
2017-03-01 10:40:41 +0100

    création server, lier fichier public au seveur, création formulaire, création dossier data

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
 // write you're code here Good Luck
+var express = require("express");
+var app = express();
+var path = require("path")
+var bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+app.get("/", function(req, res){
+	res.sendFiles(path.join("/public/index.html"));
+});
+
+app.listen(1337, function(){
+	console.log("coucou")
+})
